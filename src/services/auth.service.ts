04d64a8af56969c607ec6fab541293c16692543d
Farshid Ahsan <arnob.farshid@gmail.com>
2024-01-24 01:40:25 +0600

    add

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -65,7 +65,7 @@ export const removeCartAmountInfo = (key: string) => {
 
 export const getNewAccessToken = async () => {
   return await axiosInstance({
-    url: `http://localhost:5000/api/v1/auth/refresh-token`,
+    url: `https://alyals-backend.vercel.app/api/v1/auth/refresh-token`,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
